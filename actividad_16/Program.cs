8f4920e4749ff1c19f5d89f55d46e65e37194731
valemancilla <217262158+valemancilla@users.noreply.github.com>
2026-03-21 10:49:52 -0500

    feat: :sparkles: actividad_16
    se realizo la actividad numero 16 pedido por el docente de acuerdo a las instrucciones del documento

---
+++ actividad_16/Program.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> numeros = new List<int>();
+
+        Console.WriteLine("Ingrese 8 números:");
+        for (int i = 0; i < 8; i++)
+        {
+            Console.Write($"Número {i + 1}: ");
+            numeros.Add(int.Parse(Console.ReadLine()!));
+        }
+
+        int mayor = numeros[0];
+        foreach (int n in numeros)
+        {
+            if (n > mayor)
+                mayor = n;
+        }
+
+        int menor = numeros[0];
+        foreach (int n in numeros)
+        {
+            if (n < menor)
+                menor = n;
+        }
+
+        int suma = 0;
+        foreach (int n in numeros)
+        {
+            suma += n;
+        }
+
+        double promedio = suma / 8.0;
+
+        Console.WriteLine($"Número mayor: {mayor}");
+        Console.WriteLine($"Número menor: {menor}");
+        Console.WriteLine($"Promedio: {promedio}");
+    }
+}
